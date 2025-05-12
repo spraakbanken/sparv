19ac2b19c03edc5e3db656e5a8d0228cf9740295
Martin Hammarstedt <martin.hammarstedt@gu.se>
2025-05-12 17:00:01 +0200

    Improve code and docstrings in readability module

--- sparv/modules/readability/readability.py
+++ sparv/modules/readability/readability.py
@@ -1,5 +1,6 @@
 """Calculate readability measures."""
 
+from collections.abc import Iterable
 from math import log
 
 from sparv.api import Annotation, Output, annotator, get_logger
@@ -13,9 +14,19 @@ def lix(text: Annotation = Annotation("<text>"),
         word: Annotation = Annotation("<token:word>"),
         pos: Annotation = Annotation("<token:pos>"),
         out: Output = Output("<text>:readability.lix", description="LIX values for text chunks"),
-        skip_pos: list[str] = ["MAD", "MID", "PAD"],
-        fmt: str = "%.2f"):
-    """Create LIX annotation for text."""
+        skip_pos: Iterable[str] = ("MAD", "MID", "PAD"),
+        fmt: str = "%.2f") -> None:
+    """Create LIX annotation for text.
+
+    Args:
+        text: Text annotation.
+        sentence: Sentence annotation.
+        word: Word annotation.
+        pos: POS annotation.
+        out: Output annotation for LIX values.
+        skip_pos: List of POS to skip (e.g. punctuation).
+        fmt: Format string for output values.
+    """
     # Read annotation files and get parent_children relations
     text_children, _orphans = text.get_children(sentence)
     logger.progress(total=len(text_children) + 1)
@@ -25,9 +36,9 @@ def lix(text: Annotation = Annotation("<text>"),
 
     # Calculate LIX for every text element
     lix_annotation = []
-    for text in text_children:
+    for text_chunk in text_children:
         in_sentences = []
-        for sentence_index in text:
+        for sentence_index in text_chunk:
             s = sentence_children[sentence_index]
             in_sentences.append(list(actual_words([word_pos[token_index] for token_index in s], skip_pos)))
         lix_annotation.append(fmt % lix_calc(in_sentences))
@@ -37,13 +48,19 @@ def lix(text: Annotation = Annotation("<text>"),
     logger.progress()
 
 
-def lix_calc(sentences):
-    """
-    Calculate LIX, assuming that all tokens are actual words, not punctuation or delimiters.
+def lix_calc(sentences: list[list[str]]) -> float:
+    """Calculate LIX, assuming that all tokens are actual words, not punctuation or delimiters.
+
+    Args:
+        sentences: List of sentences, where each sentence is a list of words.
+
+    Returns:
+        LIX value as a float.
 
     >>> print("%.2f" % lix_calc(4*["a bc def ghij klmno pqrstu vxyzåäö".split()]))
     21.29
     """
+    long_word_limit = 6
     sentence_counter = 0.0
     word_counter = 0.0
     length_counter = 0.0
@@ -51,13 +68,12 @@ def lix_calc(sentences):
         sentence_counter += 1
         for word in words:
             word_counter += 1
-            length_counter += int(len(word) > 6)
+            length_counter += int(len(word) > long_word_limit)
     if word_counter == 0 and sentence_counter == 0:
         return float("NaN")
-    elif word_counter == 0 or sentence_counter == 0:
+    if word_counter == 0 or sentence_counter == 0:
         return float("inf")
-    else:
-        return word_counter / sentence_counter + 100 * length_counter / word_counter
+    return word_counter / sentence_counter + 100 * length_counter / word_counter
 
 
 @annotator("Annotate text chunks with OVIX values")
@@ -65,8 +81,8 @@ def ovix(text: Annotation = Annotation("<text>"),
          word: Annotation = Annotation("<token:word>"),
          pos: Annotation = Annotation("<token:pos>"),
          out: Output = Output("<text>:readability.ovix", description="OVIX values for text chunks"),
-         skip_pos: list[str] = ["MAD", "MID", "PAD"],
-         fmt: str = "%.2f"):
+         skip_pos: Iterable[str] = ("MAD", "MID", "PAD"),
+         fmt: str = "%.2f") -> None:
     """Create OVIX annotation for text."""
     text_children, _orphans = text.get_children(word)
     logger.progress(total=len(text_children) + 1)
@@ -74,8 +90,8 @@ def ovix(text: Annotation = Annotation("<text>"),
 
     # Calculate OVIX for every text element
     ovix_annotation = []
-    for text in text_children:
-        in_words = list(actual_words([word_pos[token_index] for token_index in text], skip_pos))
+    for text_chunk in text_children:
+        in_words = list(actual_words([word_pos[token_index] for token_index in text_chunk], skip_pos))
         ovix_annotation.append(fmt % ovix_calc(in_words))
         logger.progress()
 
@@ -83,11 +99,16 @@ def ovix(text: Annotation = Annotation("<text>"),
     logger.progress()
 
 
-def ovix_calc(words):
-    """
-    Calculate OVIX, assuming that all tokens are actual words, not punctuation or delimiters.
+def ovix_calc(words: list[str]) -> float:
+    """Calculate OVIX, assuming that all tokens are actual words, not punctuation or delimiters.
 
     Words are compared ignoring case.
+
+    Args:
+        words: List of words.
+
+    Returns:
+        OVIX value as a float.
 
     >>> for i in range(5):
     ...     print("%.2f" % ovix_calc((i*"a bc def ghij klmno pqrstu vxyzåäö ").split()))
@@ -101,26 +122,25 @@ def ovix_calc(words):
     w = 0.0
     uw = 0.0
     for word in words:
-        word = word.lower()
+        word = word.lower()  # noqa: PLW2901
         w += 1
         if word not in seen:
             seen.add(word)
             uw += 1
     if w == 0:
         return float("NaN")
-    elif uw == w:
+    if uw == w:
         return float("inf")
-    else:
-        return log(w) / log(2 - log(uw) / log(w))
+    return log(w) / log(2 - log(uw) / log(w))
 
 
 @annotator("Annotate text chunks with nominal ratios")
 def nominal_ratio(text: Annotation = Annotation("<text>"),
                   pos: Annotation = Annotation("<token:pos>"),
                   out: Output = Output("<text>:readability.nk", description="Nominal ratios for text chunks"),
-                  noun_pos: list[str] = ["NN", "PP", "PC"],
-                  verb_pos: list[str] = ["PN", "AB", "VB"],
-                  fmt: str = "%.2f"):
+                  noun_pos: Iterable[str] = ("NN", "PP", "PC"),
+                  verb_pos: Iterable[str] = ("PN", "AB", "VB"),
+                  fmt: str = "%.2f") -> None:
     """Create nominal ratio annotation for text."""
     text_children, _orphans = text.get_children(pos)
     logger.progress(total=len(text_children) + 1)
@@ -128,25 +148,34 @@ def nominal_ratio(text: Annotation = Annotation("<text>"),
 
     # Calculate OVIX for every text element
     nk_annotation = []
-    for text in text_children:
-        in_pos = [pos_annotation[token_index] for token_index in text]
+    for text_chunk in text_children:
+        in_pos = [pos_annotation[token_index] for token_index in text_chunk]
         nk_annotation.append(fmt % nominal_ratio_calc(in_pos, noun_pos, verb_pos))
         logger.progress()
     out.write(nk_annotation)
     logger.progress()
 
 
-def nominal_ratio_calc(pos: list[str], noun_pos: list[str], verb_pos: list[str]):
-    """
-    Calculate nominal ratio (nominalkvot).
+def nominal_ratio_calc(pos: list[str], noun_pos: Iterable[str], verb_pos: Iterable[str]) -> float:
+    """Calculate nominal ratio (nominalkvot).
+
+    Args:
+        pos: List of POS tags.
+        noun_pos: List of POS tags for nouns.
+        verb_pos: List of POS tags for verbs.
+
+    Returns:
+        Nominal ratio as a float.
 
     >>> "%.1f" % nominal_ratio_calc('NN JJ'.split(), noun_pos="NN PP PC".split(), verb_pos="PN AB VB".split())
     'inf'
     >>> "%.1f" % nominal_ratio_calc('NN NN VB'.split(), noun_pos="NN PP PC".split(), verb_pos="PN AB VB".split())
     '2.0'
-    >>> "%.1f" % nominal_ratio_calc('NN PP PC PN AB VB MAD MID'.split(), noun_pos="NN PP PC".split(), verb_pos="PN AB VB".split())
+    >>> "%.1f" % nominal_ratio_calc('NN PP PC PN AB VB MAD MID'.split(), noun_pos="NN PP PC".split(), \
+        verb_pos="PN AB VB".split())
     '1.0'
-    >>> "%.1f" % nominal_ratio_calc('NN AB VB PP PN PN MAD'.split(), noun_pos="NN PP PC".split(), verb_pos="PN AB VB".split())
+    >>> "%.1f" % nominal_ratio_calc('NN AB VB PP PN PN MAD'.split(), noun_pos="NN PP PC".split(), \
+        verb_pos="PN AB VB".split())
     '0.5'
     >>> "%.1f" % nominal_ratio_calc('RG VB'.split(), noun_pos="NN PP PC".split(), verb_pos="PN AB VB".split())
     '0.0'
@@ -161,9 +190,15 @@ def nominal_ratio_calc(pos: list[str], noun_pos: list[str], verb_pos: list[str])
         return float("inf")
 
 
-def actual_words(cols, skip_pos: list[str]):
-    """
-    Remove words with punctuation and delimiter POS (provided by skip_pos).
+def actual_words(cols: Iterable[tuple[str, str]], skip_pos: Iterable[str]) -> Iterable[str]:
+    """Remove words with punctuation and delimiter POS (provided by skip_pos).
+
+    Args:
+        cols: List of tuples with word and POS.
+        skip_pos: List of POS to skip.
+
+    Yields:
+        Actual words (not punctuation or delimiters).
 
     >>> ' '.join(actual_words(
     ...     [('Hej', 'IN'),
