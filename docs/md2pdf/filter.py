1fffa8796ad22825a5c69320942e5862278718f6
Anne Schumacher <anne.schumacher@gu.se>
2025-05-13 15:07:47 +0200

    make PDF generation work for mkdocs admonitions and improve script

--- docs/md2pdf/filter.py
+++ docs/md2pdf/filter.py
@@ -1,23 +1,39 @@
 """Pandoc filter for removing links that won't work in PDF and reformatting admonitions.
 
 https://pandoc.org/filters.html
+
+
+Note: When debugging, print to stderr (e.g. print(value, file=sys.stderr)) as printing to stdout will break
+pandocfilters.
 """
 
 from __future__ import annotations
 
 from typing import Any
 
-from pandocfilters import BlockQuote, Str, Strong, toJSONFilter
+from pandocfilters import BlockQuote, Para, Str, Strong, toJSONFilter
+
+
+class AdmonitionTracker:
+    """Class to track the state of the previous admonition."""
+
+    def __init__(self) -> None:
+        """Initialize the tracker with no previous admonition."""
+        self.previous_admonition = None
+
+
+tracker = AdmonitionTracker()
 
 
 def fix_document(key: str, value: str | list | dict | None, _format: str, _meta: dict) -> Any | None:
     """Remove links that won't work in PDF and reformat block quotes."""  # noqa: DOC201
+    # Remove internal links, just keep the text
     if key == "Link":
         url = value[2][0]
         if url.startswith(("user-manual", "developers-guide")):
-            # Return the link text
             return value[1]
-    # Reformat the text inside block quotes
+
+    # Reformat the text inside block quotes (standard admonitions)
     elif key == "BlockQuote":
         try:
             first_string = value[0]["c"][0]["c"]
@@ -38,6 +54,30 @@ def fix_document(key: str, value: str | list | dict | None, _format: str, _meta:
                 return BlockQuote(value)
         except Exception:
             return None
+
+    # Check for Para starting with "!!!" (mkdocs-style admonitions)
+    elif key == "Para":
+        if len(value) > 1 and value[0]["t"] == "Str" and value[0]["c"] == "!!!":
+            # Remove the "!!!" and format the first element (capitalize and make it bold)
+            value = value[2:]
+            admonition_title = Str(value[0]["c"].capitalize() + ": ")
+            value[0] = Strong([admonition_title])
+
+            # Admonition with title: search for (Quoted), remove quotes and make it bold
+            for item in value:
+                if isinstance(item, dict) and item.get("t") == "Quoted":
+                    tracker.previous_admonition = Strong([admonition_title, *item["c"][1]])
+                    return []  # Return an empty list to remove the element
+
+            # Admonition without title, convert to BlockQuote
+            return BlockQuote([Para(value)])
+
+    # Check if the current element is a CodeBlock and the previous element was an admonition
+    elif key == "CodeBlock" and tracker.previous_admonition is not None:
+        admonition = BlockQuote([Para([tracker.previous_admonition]), Para([Str(value[1])])])
+        tracker.previous_admonition = None
+        return admonition
+
     return None
 
 
--- docs/md2pdf/make_pdf.sh
+++ docs/md2pdf/make_pdf.sh
@@ -1,40 +1,17 @@
 #!/usr/bin/env bash
-# set -x
+set -euo pipefail  # Exit on error, unset variables, and pipe failures
+IFS=$'\n\t'        # Set safe IFS
 
-# Script for creating PDFs from markdown
-# Requires markdown, latex and Python pandocfilters
-
-USER_MANUAL_FILES=$(grep -P 'user-manual/.*\.md' ../mkdocs.yml -o | grep -v 'intro\.md' | sed 's/^/..\//')
-
-DEVELOPERS_GUIDE_FILES=$(grep -P 'developers-guide/.*\.md' ../mkdocs.yml -o | sed 's/^/..\//')
+# Script for creating PDFs from markdown using Pandoc
+# Requires markdown, pandoc, latex, and Python pandocfilters
 
 # Get version number from sparv/__init__.py
 SPARV_VERSION=$(grep -P '(?<=__version__ = ").+(?=")' -o ../../sparv/__init__.py)
 
-function make_pandoc {
-    # Convert markdown to latex/pdf:
-    # pandoc -t latex -o $1.tex $1.md \
-    # pandoc -t native $1.md \
-    pandoc -t latex -o $1.pdf $1.md \
-    --filter filter.py \
-    -H settings_template.tex `# include in header` \
-    --template template.tex `# use template`  \
-    --toc `# table of contents` \
-    -N `# numbered sections` \
-    -V urlcolor=RoyalBlue `# color links blue` \
-    --listings `# use listings package for LaTeX code blocks`
-    #-V links-as-notes=true `# print links as footnotes` \
-}
-
-
-function make_document {
-    # $1: file name (without extension)
-    # $2: markdown file list
-    # $3: Title string
-
-    HEADER="
+# Title page for the PDF (not used in GU-ISS publication)
+TITLEPAGE="
 ---
-title: Sparv $SPARV_VERSION - $3
+title: Sparv $SPARV_VERSION - Documentation
 author: |
   | Språkbanken Text
   | Institutionen för svenska, flerspråkighet och språkteknologi
@@ -45,32 +22,141 @@ author: |
   |
   | ![](../images/sparv_detailed.png){width=2.5cm}
 ---
-    "
+"
 
-    # Concat header and files
-    echo -e "$HEADER" > $1.md
-    for f in $2
-    do
-      cat $f >> $1.md
-      echo -e "\n" >> $1.md
-    done
-    make_pandoc $1
+# Create output directory
+OUTPUT_DIR="output"
+mkdir -p "$OUTPUT_DIR"
 
-    # Make GU-ISS publication without header
-    for f in $2
-    do
-      cat $f >> GU-ISS-$1.md
-      echo -e "\n" >> GU-ISS-$1.md
-    done
-    make_pandoc GU-ISS-$1
+# Function for converting markdown to pdf with Pandoc
+function markdown_to_pdf {
+    local input_file=$1
+    local mode=$2
+
+    case $mode in
+        tex)
+            # Convert to .tex for debugging
+            pandoc -t latex -o "${OUTPUT_DIR}/${input_file}.tex" "${OUTPUT_DIR}/${input_file}.md"
+            ;;
+        pandoc)
+            # Convert to pandoc's native format for debugging filters
+            pandoc -t native "${OUTPUT_DIR}/${input_file}.md" -o "${OUTPUT_DIR}/${input_file}.pandoc"
+            ;;
+        pdf)
+            # Convert markdown to latex/pdf
+            pandoc -t latex -o "${OUTPUT_DIR}/${input_file}.pdf" "${OUTPUT_DIR}/${input_file}.md" \
+                --filter filter.py \
+                -H settings_template.tex `# include in header` \
+                --template template.tex `# use template` \
+                --toc `# table of contents` \
+                --top-level-division=chapter `# treat top-level headings as chapters` \
+                -N `# numbered sections` \
+                -V urlcolor=RoyalBlue `# color links blue` \
+                --listings `# use listings package for LaTeX code blocks`
+            ;;
+        *)
+            echo "Invalid mode: $mode. Use 'tex', 'native', or 'pdf'."
+            exit 1
+            ;;
+    esac
 }
 
-# Make PDFs
-make_document user-manual "$USER_MANUAL_FILES" "User Manual"
-make_document developers-guide "$DEVELOPERS_GUIDE_FILES" "Developer's Guide"
+# Function to generate PDF with or without title page
+function generate_pdf {
+    local titlepage=$1
+    local mode=$2
+    local output_file
 
-# Clean-up
-rm user-manual.md
-rm developers-guide.md
-rm GU-ISS-user-manual.md
-rm GU-ISS-developers-guide.md
+    # Define file mappings for the two parts of the documentation
+    declare -A files=(
+        ["user-manual.md"]="$(grep -P 'user-manual/.*\.md' ../mkdocs.yml -o | grep -v 'intro\.md' | sed 's/^/..\//')"
+        ["dev-guide.md"]="$(grep -P 'developers-guide/.*\.md' ../mkdocs.yml -o | sed 's/^/..\//')"
+    )
+
+    # Concat files and shift headings for each document
+    for output in "${!files[@]}"; do
+        # Clear output file
+        echo "" > "${OUTPUT_DIR}/${output}"
+        for f in ${files[$output]}; do
+            cat "$f" >> "${OUTPUT_DIR}/${output}"
+            echo -e "\n" >> "${OUTPUT_DIR}/${output}"
+        done
+        pandoc "${OUTPUT_DIR}/${output}" --shift-heading-level-by=1 -o "${OUTPUT_DIR}/${output}"
+    done
+
+    # Add title page to the output file if needed
+    if [ "$titlepage" = true ]; then
+        output_file="sparv-documentation"
+        echo -e "$TITLEPAGE" > "${OUTPUT_DIR}/${output_file}.md"
+    else
+        output_file="GU-ISS-sparv-documentation"
+        echo "" > "${OUTPUT_DIR}/${output_file}.md"
+    fi
+
+    # Concatenate the user manual and developer's guide and convert to PDF
+    echo -e "# User Manual\n" >> "${OUTPUT_DIR}/${output_file}.md"
+    cat "${OUTPUT_DIR}/user-manual.md" >> "${OUTPUT_DIR}/${output_file}.md"
+    echo "\newpage" >> "${OUTPUT_DIR}/${output_file}.md"
+    echo -e "# Developer's Guide\n" >> "${OUTPUT_DIR}/${output_file}.md"
+    cat "${OUTPUT_DIR}/dev-guide.md" >> "${OUTPUT_DIR}/${output_file}.md"
+
+    # If MODE == md skip running pandoc
+    if [ "$mode" = "md" ]; then
+        return
+    fi
+    markdown_to_pdf "$output_file" "$mode"
+}
+
+# Parse command-line arguments
+MODE="pdf"  # Default mode
+while getopts "m:h" opt; do
+    case $opt in
+        m)
+            MODE=$OPTARG
+            ;;
+        h)
+            echo "Usage: $0 [-m mode] (mode: 'tex', 'pandoc', 'pdf')"
+            echo "  -m mode   Specify the output format:"
+            echo "            'tex'    Generate .tex files for debugging."
+            echo "            'pandoc' Generate .pandoc files for debugging filters."
+            echo "            'md'     Generate .md files."
+            echo "            'pdf'    Generate PDF files (default)."
+            echo "  -h        Display this help message."
+            exit 0
+            ;;
+        *)
+            echo "Invalid option. Use -h for help."
+            exit 1
+            ;;
+    esac
+done
+
+# Generate PDFs (or .tex/.pandoc files) with and without title page
+generate_pdf true "$MODE"
+generate_pdf false "$MODE"
+
+# Clean up intermediate files
+if [ "$MODE" != "md" ]; then
+    rm -f ${OUTPUT_DIR}/*.md
+fi
+
+# Print message about created files
+echo "The following files have been created in the '${OUTPUT_DIR}' directory:"
+case $MODE in
+    tex)
+        echo "- sparv-documentation.tex"
+        echo "- GU-ISS-sparv-documentation.tex"
+        ;;
+    pandoc)
+        echo "- sparv-documentation.pandoc"
+        echo "- GU-ISS-sparv-documentation.pandoc"
+        ;;
+    md)
+        echo "- sparv-documentation.md"
+        echo "- GU-ISS-sparv-documentation.md"
+        ;;
+    pdf)
+        echo "- sparv-documentation.pdf"
+        echo "- GU-ISS-sparv-documentation.pdf"
+        ;;
+esac
